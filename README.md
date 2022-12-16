802b34fe73895742623debd6bc283fa44c462aa2
hyejinU <gpwls4306@g.skku.edu>
2022-12-16 19:17:03 +0900

    /hello

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # skku-sw-winter
+
+### Hello World
+
+-------- 
+
+# how to run server?
+
+`cd build/libs`
+
+`java -jar helloWorld-0.0.1-SNAPSHOT.jar`
+
+link to <http://localhost:8080/hello>
---
+++ src/main/java/com/example/helloworld/web/HelloWorldController.java
@@ -0,0 +1,11 @@
+package com.example.helloworld.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+    @GetMapping("/hello")
+    public String hello() { return "Hello world";}
+
+}
